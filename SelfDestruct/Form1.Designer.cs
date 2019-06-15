2217c3691127dee904275ea3c76b6586a412c7de
BettTheTimeTraveller <bettthetimetraveller@gmail.com>
2019-06-15 11:08:24 +0300

    first commit

---
+++ SelfDestruct/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SelfDestruct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.killCode = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sdsLbl = new System.Windows.Forms.Label();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.displayLbl = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.killCode);
+            this.bunifuGradientPanel1.Controls.Add(this.label1);
+            this.bunifuGradientPanel1.Controls.Add(this.sdsLbl);
+            this.bunifuGradientPanel1.Controls.Add(this.player);
+            this.bunifuGradientPanel1.Controls.Add(this.displayLbl);
+            this.bunifuGradientPanel1.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.Black;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(921, 454);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // killCode
+            // 
+            this.killCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.killCode.BackColor = System.Drawing.Color.Black;
+            this.killCode.BorderColorFocused = System.Drawing.Color.Turquoise;
+            this.killCode.BorderColorIdle = System.Drawing.Color.Turquoise;
+            this.killCode.BorderColorMouseHover = System.Drawing.Color.Turquoise;
+            this.killCode.BorderThickness = 3;
+            this.killCode.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.killCode.Font = new System.Drawing.Font("Dungeon", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.killCode.ForeColor = System.Drawing.Color.Turquoise;
+            this.killCode.isPassword = true;
+            this.killCode.Location = new System.Drawing.Point(253, 337);
+            this.killCode.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.killCode.Name = "killCode";
+            this.killCode.Size = new System.Drawing.Size(414, 54);
+            this.killCode.TabIndex = 5;
+            this.killCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.killCode.OnValueChanged += new System.EventHandler(this.killCode_OnValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Dungeon", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Turquoise;
+            this.label1.Location = new System.Drawing.Point(335, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Kill Code:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sdsLbl
+            // 
+            this.sdsLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sdsLbl.BackColor = System.Drawing.Color.Transparent;
+            this.sdsLbl.Font = new System.Drawing.Font("Dungeon", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sdsLbl.ForeColor = System.Drawing.Color.Turquoise;
+            this.sdsLbl.Location = new System.Drawing.Point(10, 40);
+            this.sdsLbl.Name = "sdsLbl";
+            this.sdsLbl.Size = new System.Drawing.Size(900, 95);
+            this.sdsLbl.TabIndex = 3;
+            this.sdsLbl.Text = "Self Destruct Sequence Initiated";
+            this.sdsLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player
+            // 
+            this.player.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(329, 409);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(262, 33);
+            this.player.TabIndex = 2;
+            // 
+            // displayLbl
+            // 
+            this.displayLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayLbl.BackColor = System.Drawing.Color.Transparent;
+            this.displayLbl.Font = new System.Drawing.Font("Digital-7 Mono", 99.74999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayLbl.ForeColor = System.Drawing.Color.Red;
+            this.displayLbl.Location = new System.Drawing.Point(92, 135);
+            this.displayLbl.Name = "displayLbl";
+            this.displayLbl.Size = new System.Drawing.Size(736, 179);
+            this.displayLbl.TabIndex = 0;
+            this.displayLbl.Text = "00:00";
+            this.displayLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 454);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Label displayLbl;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Timer timer;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Label sdsLbl;
+        private Bunifu.Framework.UI.BunifuMetroTextbox killCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SelfDestruct/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace SelfDestruct
+{
+    public partial class Form1 : Form
+    {
+        private int seconds, minutes, key = 0, token, keyClose = 0;
+        private string killerCode = "12";
+        public Form1()
+        {
+            this.seconds = 22;
+            this.minutes = 0;
+            InitializeComponent();
+            player.Hide();
+            player.URL = "./tones/initiated.mp3";
+            display();
+            timer.Start();
+        }
+
+        private void display()
+        {
+            string ds, dm;
+            if (this.seconds < 10)
+            {
+                ds = "0" + this.seconds.ToString();
+            }
+            else
+            {
+                ds = this.seconds.ToString();
+            }
+            if (this.minutes < 10)
+            {
+                dm = "0" + this.minutes.ToString();
+            }
+            else
+            {
+                dm = this.minutes.ToString();
+            }
+            
+
+            displayLbl.Text = dm + ":" + ds;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.countDown();
+            if (this.minutes == 0 && this.seconds == 0)
+            {
+                timer.Stop();
+                Application.Exit();
+            }
+            if (this.minutes * this.seconds <= 8 || this.minutes * this.seconds == 0 && !(minutes == 0 && seconds < 20))
+            {
+                if(this.killerCode.Length <= 12)
+                {
+                    //this.loading(this.seconds);
+                    sdsLbl.Hide();
+                    this.display();
+                    if (minutes == 0 && seconds < 20 && seconds>= 15)
+                    {
+                        Console.Beep(3500, 90);
+                        Console.Beep(3500, 90);
+                  
+                    }
+                    else if (minutes == 0 && seconds < 15 && seconds >= 10)
+                    {
+                        Console.Beep(3500, 170);
+                        Console.Beep(3500, 170);
+                        Console.Beep(3500, 170);
+                    }
+                    else if (minutes == 0 && seconds < 10 && seconds >= 5)
+                    {
+                        Console.Beep(3500, 200);
+                        Console.Beep(3500, 200);
+                        Console.Beep(3500, 200);
+                        Console.Beep(3500, 200);
+                        Console.Beep(3500, 200);
+                    }
+                    else if (minutes == 0 && seconds < 5)
+                    {
+                        if (keyClose == 0)
+                        {
+                            Console.Beep(3500, 200);
+                            keyClose = 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.Beep(1500, 100);
+                    }
+                    
+                }
+                else
+                {
+                    if (this.key == 0)
+                    {
+                        player.URL = "./tones/terminated.mp3";
+                        sdsLbl.Show();
+                        sdsLbl.Text = "Self Destruct Sequence Terminated";
+                        if (this.minutes < 1)
+                        {
+                            this.token = this.seconds;
+                        }
+                        else
+                        {
+                            this.token = 60 + this.seconds;
+                        }
+                        this.key = 1;
+                    }
+                    if (this.token - this.second(this.minutes, this.seconds) > 3)
+                    {
+                        timer.Stop();
+                        Application.Exit();
+                    }
+                }
+            }
+            else 
+            {
+                Console.Beep(1500, 100);
+                this.display();
+            }
+            
+        }
+
+        private void loading(int s)
+        {
+            if (s % 4 == 0)
+            {
+                sdsLbl.Text = "Self Destruct Sequence Initiated...";
+            }
+            else if(s % 4 == 1)
+            {
+                sdsLbl.Text = "Self Destruct Sequence Initiated..";
+            }
+            else if (s % 4 == 2)
+            {
+                sdsLbl.Text = "Self Destruct Sequence Initiated.";
+            }
+            else
+            {
+                sdsLbl.Text = "Self Destruct Sequence Initiated";
+            }
+        }
+
+        private void countDown()
+        {
+            this.seconds -= 1;
+            if (this.seconds < 0)
+            {
+                this.seconds = 59;
+                this.minutes -= 1;
+            }
+        }
+
+        private int second(int min, int sec)
+        {
+            if(min < 1)
+            {
+                return sec;
+            }
+            else
+            {
+                return 60 + sec;
+            }
+        }
+
+        private void killCode_OnValueChanged(object sender, EventArgs e)
+        {
+            //this.killerCode = killCode.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
